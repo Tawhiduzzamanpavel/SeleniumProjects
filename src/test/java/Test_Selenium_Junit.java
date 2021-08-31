8d33e9a050f0bec14d3640f130fb892bed62e4e3
Tawhiduzzaman <tawhid239@gmail.com>
2021-08-31 12:32:36 +0600

    upload

---
+++ src/test/java/Test_Selenium_Junit.java
@@ -0,0 +1,37 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+//import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test_Selenium_Junit {
+    WebDriver driver ;
+    @Before
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", "H:/SDET/SDET_Projects/Selenium_Junit/src/test/resources/chromedriver.exe");
+        ChromeOptions ops=new ChromeOptions();
+        ops.addArguments("--headed");
+        driver=new ChromeDriver(ops);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+    @Test
+    public void getTitle(){
+        driver.get("https://demoqa.com ");
+        String title=driver.getTitle();
+        System.out.println(title);
+        Assert.assertTrue(title.contains("ToolsQA"));
+    }
+    @After
+    public void finishTest(){
+        driver.close();
+    }
+
+}
